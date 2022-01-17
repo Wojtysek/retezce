e96fba6ab3cdc94f19e17fec527638c40ac48c61
Vojtech Hruby <vojtech.hruby.020@pslib.cz>
2022-01-17 14:17:12 +0100

    final

--- retezce/retezce.cpp
+++ retezce/retezce.cpp
@@ -3,7 +3,12 @@
 
 int main(void) {
 
-	char text[] = "jested";
+	char text[] = "vojtisek";
+	char druha[] = "hruby";
 	printf("%s\n", text);
 	printf("delka: %d\n", strlen(text));
+	char buffer[1024];
+	strcpy(buffer, text);
+	strcat(buffer, druha);
+	printf("%s\n", buffer);
 }
